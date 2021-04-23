0787829a64d6cac2d00aabb1eb5e861232d707c2
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2021-04-23 21:13:54 +0300

    Add mm files

---
+++ Navigation/GeorgChronics.mm
@@ -0,0 +1,49 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#99ff99" CREATED="1618471616357" ID="ID_1641934503" MODIFIED="1618471971701" TEXT="&#x420;&#x443;&#x431;&#x440;&#x438;&#x43a;&#x430;&#x442;&#x43e;&#x440; &#x425;&#x440;&#x43e;&#x43d;&#x438;&#x43a; &#x413;&#x435;&#x43e;&#x440;&#x433;&#x430;">
+<icon BUILTIN="full-1"/>
+<node CREATED="1618471650826" ID="ID_494564736" MODIFIED="1618471859159" POSITION="right" TEXT="&#x41a;&#x43e;&#x43c;&#x430;&#x43d;&#x434;&#x438;&#x440;&#x43e;&#x432;&#x43a;&#x438;/&#x43f;&#x43e;&#x435;&#x437;&#x434;&#x43a;&#x438;">
+<icon BUILTIN="full-2"/>
+<node CREATED="1618471904365" ID="ID_1613624260" MODIFIED="1618471957145" TEXT="&#x41a;&#x43e;&#x43c;&#x430;&#x43d;&#x434;&#x438;&#x440;&#x43e;&#x432;&#x43a;&#x438; &#x438; &#x440;&#x430;&#x431;&#x43e;&#x447;&#x438;&#x435; &#x43f;&#x43e;&#x435;&#x437;&#x434;&#x43a;&#x438;">
+<arrowlink DESTINATION="ID_1613624260" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_756560592" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<arrowlink DESTINATION="ID_1613624260" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1557260419" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1613624260" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_756560592" SOURCE="ID_1613624260" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1613624260" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1557260419" SOURCE="ID_1613624260" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<icon BUILTIN="full-3"/>
+<node BACKGROUND_COLOR="#ccffcc" CREATED="1618472002537" ID="ID_1591373811" LINK="https://zen.yandex.ru/media/id/5d0dba97ecd5cf00afaf2938/kratkii-obzor-nashei-komandirovki-v-nijnii-novgorod-711-aprelia-6075ecd94101d26252073fff" MODIFIED="1618472313715" TEXT="&#x41a;&#x43e;&#x43c;&#x430;&#x43d;&#x434;&#x438;&#x440;&#x43e;&#x432;&#x43a;&#x430; &#x432; &#x41d;&#x438;&#x436;&#x43d;&#x438;&#x439; &#x41d;&#x43e;&#x432;&#x433;&#x43e;&#x440;&#x43e;&#x434; &#x432; &#x441;&#x43e;&#x441;&#x442;&#x430;&#x432;&#x435; VC-Team &#x434;&#x43b;&#x44f; &#x437;&#x430;&#x432;&#x435;&#x440;&#x448;&#x435;&#x43d;&#x438;&#x44f; &#x43a;&#x443;&#x440;&#x441;&#x430; &#x43f;&#x43e; BIM-&#x41c;&#x435;&#x43d;&#x435;&#x434;&#x436;&#x43c;&#x435;&#x43d;&#x442;&#x443; 2020-2021 "/>
+</node>
+<node CREATED="1618471919304" ID="ID_1494113124" MODIFIED="1618471953761" TEXT="&#x421;&#x432;&#x43e;&#x438; &#x432;&#x44b;&#x43b;&#x430;&#x437;&#x43a;&#x438; &#x43d;&#x430; &#x43f;&#x440;&#x438;&#x440;&#x43e;&#x434;&#x443;">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+<node CREATED="1618471665278" ID="ID_1146312426" MODIFIED="1618472318015" POSITION="left" TEXT="&#x41f;&#x440;&#x43e;&#x444;&#x435;&#x441;&#x441;&#x438;&#x43e;&#x43d;&#x430;&#x43b;&#x44c;&#x43d;&#x44b;&#x435; &#x441;&#x442;&#x430;&#x442;&#x44c;&#x438;">
+<icon BUILTIN="full-2"/>
+<node CREATED="1618471676549" ID="ID_890979792" MODIFIED="1618471865771" TEXT="&#x41e; &#x43f;&#x440;&#x438;&#x43a;&#x43b;&#x430;&#x434;&#x43d;&#x43e;&#x43c; &#x43f;&#x440;&#x43e;&#x433;&#x440;&#x430;&#x43c;&#x43c;&#x438;&#x440;&#x43e;&#x432;&#x430;&#x43d;&#x438;&#x438;">
+<icon BUILTIN="full-3"/>
+<node BACKGROUND_COLOR="#ccffff" CREATED="1618472241354" ID="ID_1067518957" LINK="https://zen.yandex.ru/media/id/5d0dba97ecd5cf00afaf2938/zanimatelnoe-programmirovanie-eksport-oblaka-tochek-v-obj-format-607691bb37222b00b28e6cbc" MODIFIED="1618472301096" TEXT="&#x42d;&#x43a;&#x441;&#x43f;&#x43e;&#x440;&#x442; &#x43e;&#x431;&#x43b;&#x430;&#x43a;&#x430; &#x442;&#x43e;&#x447;&#x435;&#x43a; &#x432; &#x444;&#x43e;&#x440;&#x43c;&#x430;&#x442; OBJ"/>
+</node>
+<node CREATED="1618471713783" ID="ID_1267347593" MODIFIED="1618471869935" TEXT="&#x41e; &#x43f;&#x440;&#x43e;&#x434;&#x443;&#x43a;&#x442;&#x430;&#x445; Autodesk">
+<arrowlink DESTINATION="ID_1267347593" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_481459289" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<arrowlink DESTINATION="ID_1267347593" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1273811724" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1267347593" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_481459289" SOURCE="ID_1267347593" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1267347593" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1273811724" SOURCE="ID_1267347593" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<icon BUILTIN="full-3"/>
+<node CREATED="1618471729781" ID="ID_90571920" MODIFIED="1618471885613" TEXT="&#x421;&#x438;&#x441;&#x442;&#x435;&#x43c;&#x44b; &#x43a;&#x43e;&#x43e;&#x440;&#x434;&#x438;&#x43d;&#x430;&#x442;">
+<icon BUILTIN="full-4"/>
+<node CREATED="1618736499325" ID="ID_1205822912" LINK="https://zen.yandex.ru/media/id/5d0dba97ecd5cf00afaf2938/pogrujaemsia-v-sistemy-koordinat-chast-1-kak-vse-nachinalos-607935a4af40cb228043fd3c" MODIFIED="1618736522610" TEXT="&#x41f;&#x43e;&#x433;&#x440;&#x443;&#x436;&#x430;&#x435;&#x43c;&#x441;&#x44f; &#x432; &#x441;&#x438;&#x441;&#x442;&#x435;&#x43c;&#x44b; &#x43a;&#x43e;&#x43e;&#x440;&#x434;&#x438;&#x43d;&#x430;&#x442;. &#x427;&#x430;&#x441;&#x442;&#x44c; 1 - &#x43a;&#x430;&#x43a; &#x432;&#x441;&#x451; &#x43d;&#x430;&#x447;&#x438;&#x43d;&#x430;&#x43b;&#x43e;&#x441;&#x44c;"/>
+<node CREATED="1618736527000" ID="ID_1454575522" LINK="https://zen.yandex.ru/media/id/5d0dba97ecd5cf00afaf2938/pogrujaemsia-v-sistemy-koordinat-chast-2-ruchnoe-vnesenie-v-biblioteku-informacii-o-sk-607ac4be298124331e723f06" MODIFIED="1618736542834" TEXT="&#x41f;&#x43e;&#x433;&#x440;&#x443;&#x436;&#x430;&#x435;&#x43c;&#x441;&#x44f; &#x432; &#x441;&#x438;&#x441;&#x442;&#x435;&#x43c;&#x44b; &#x43a;&#x43e;&#x43e;&#x440;&#x434;&#x438;&#x43d;&#x430;&#x442;. &#x427;&#x430;&#x441;&#x442;&#x44c; 2 - &#x440;&#x443;&#x447;&#x43d;&#x43e;&#x435; &#x432;&#x43d;&#x435;&#x441;&#x435;&#x43d;&#x438;&#x435; &#x432; &#x431;&#x438;&#x431;&#x43b;&#x438;&#x43e;&#x442;&#x435;&#x43a;&#x443; &#x438;&#x43d;&#x444;&#x43e;&#x440;&#x43c;&#x430;&#x446;&#x438;&#x438; &#x43e; &#x421;&#x41a;"/>
+</node>
+<node CREATED="1618471741393" ID="ID_64274773" MODIFIED="1618471890135" TEXT="AutoCAD Civil 3D">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1618471766673" ID="ID_739211654" MODIFIED="1618471893767" TEXT="Autodesk InfraWorks">
+<icon BUILTIN="full-4"/>
+<node BACKGROUND_COLOR="#ccffff" CREATED="1618472201101" ID="ID_810644814" LINK="https://zen.yandex.ru/media/id/5d0dba97ecd5cf00afaf2938/autodesk-infraworks-obhodim-apparatnye-ogranicheniia-bolshih-modelei-6076d27601ff502ad0fd2b01" MODIFIED="1618472289372" TEXT="&#x41e;&#x431;&#x445;&#x43e;&#x434;&#x438;&#x43c; &#x430;&#x43f;&#x43f;&#x430;&#x440;&#x430;&#x442;&#x43d;&#x44b;&#x435; &#x43e;&#x433;&#x440;&#x430;&#x43d;&#x438;&#x447;&#x435;&#x43d;&#x438;&#x44f; &#x431;&#x43e;&#x43b;&#x44c;&#x448;&#x438;&#x445; &#x43c;&#x43e;&#x434;&#x435;&#x43b;&#x435;&#x439;"/>
+</node>
+</node>
+<node CREATED="1618471782055" ID="ID_1734689371" MODIFIED="1618471877108" TEXT="&#x41a;&#x43e;&#x43e;&#x440;&#x434;&#x438;&#x43d;&#x430;&#x446;&#x438;&#x44f; &#x43c;&#x43e;&#x434;&#x435;&#x43b;&#x435;&#x439; &#x438; &#x413;&#x418;&#x421;">
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+</node>
+</map>
